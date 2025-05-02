b3cf1a4d8da4fee0f3ad0805ed2622b0c727f18e
karel <buet.karel@gmail.com>
2025-05-02 15:24:56 +0200

    mise a jour

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,39 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className={styles.home}>
+          <div className={styles.left}>
+            <div className={styles.leftUp}>
+              <Image
+                className={styles.logo}
+                src={"/logo-twitter.png"}
+                alt={"logo-twitter.png"}
+                width={60}
+                height={60}
+              />
+            </div>
+          </div>
+          <div className={styles.center}>
+            <div className={styles.tweet_content}>
+              <div>
+                <h1 className={styles.title}>Home</h1>
+              </div>
+              <div className={styles.input_content}>
+                <input
+                  placeholder="What's up?"
+                  className={styles.tweet_input}
+                ></input>
+              </div>
+              <div className={styles.tweet_down}>
+                <button className={styles.btn_tweet}>Tweet</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.right}></div>
         </div>
       </main>
     </div>
