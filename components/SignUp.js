f20d5b64400d746b1462a10fb246f3ed074724e9
karel <buet.karel@gmail.com>
2025-05-02 14:10:14 +0200

    amelioration input

--- components/SignUp.js
+++ components/SignUp.js
@@ -27,72 +27,93 @@ function SignUp() {
   );
 
   return (
-    <div>
+    <div className={styles.content}>
       <h2 className={styles.title}>Créer votre compte</h2>
-      <form>
-        <div>
-          <div>
-            <input type="text" placeholder="Username" />
-          </div>
-          <div>
-            <input type="password" placeholder="Password" />
-          </div>
+
+      <div style={{ width: "100%" }}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <input type="text" placeholder="Username" className={styles.input} />
         </div>
-        <div>
-          <h5 className={styles.text}>Date de naissance</h5>
-          <p className={styles.text_dateNaissance}>
-            Cette information ne sera pas affichée publiquement. Confirmez votre
-            âge, même si ce compte est pour une entreprise, un animal de
-            compagnie ou autre chose.
-          </p>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.input}
+          />
         </div>
-        <div>
-          <label>
-            Month:
-            <select value={month} onChange={(e) => setMonth(e.target.value)}>
-              <option value="">--</option>
-              {months.map((m, i) => (
-                <option key={i} value={i + 1}>
-                  {m}
-                </option>
-              ))}
-            </select>
-          </label>
+      </div>
+      <div>
+        <h5 className={styles.text}>Date de naissance</h5>
+        <p className={styles.text_dateNaissance}>
+          Cette information ne sera pas affichée publiquement. Confirmez votre
+          âge, même si ce compte est pour une entreprise, un animal de compagnie
+          ou autre chose.
+        </p>
+      </div>
+      <div className={styles.contentDate}>
+        <label className={styles.dateLabel} placeholder="Password">
+          Month:
+          <select
+            value={month}
+            onChange={(e) => setMonth(e.target.value)}
+            className={styles.dateInput}
+          >
+            <option value="" disabled hidden>
+              Mois
+            </option>
+            {months.map((m, i) => (
+              <option key={i} value={i + 1}>
+                {m}
+              </option>
+            ))}
+          </select>
+        </label>
 
-          <label>
-            Day:
-            <select value={day} onChange={(e) => setDay(e.target.value)}>
-              <option value="">--</option>
-              {days.map((d) => (
-                <option key={d} value={d}>
-                  {d}
-                </option>
-              ))}
-            </select>
-          </label>
+        <label className={styles.dateLabel}>
+          Day:
+          <select
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            className={styles.dateInput}
+          >
+            <option value="" disabled hidden>
+              Jour
+            </option>
+            {days.map((d) => (
+              <option key={d} value={d}>
+                {d}
+              </option>
+            ))}
+          </select>
+        </label>
 
-          <label>
-            Year:
-            <select value={year} onChange={(e) => setYear(e.target.value)}>
-              <option value="">--</option>
-              {years.map((y) => (
-                <option key={y} value={y}>
-                  {y}
-                </option>
-              ))}
-            </select>
-          </label>
+        <label className={styles.dateLabel}>
+          Year:
+          <select
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            className={styles.dateInput}
+          >
+            <option value="" disabled hidden>
+              Année
+            </option>
+            {years.map((y) => (
+              <option key={y} value={y}>
+                {y}
+              </option>
+            ))}
+          </select>
+        </label>
 
-          <p>
-            Selected date:{" "}
-            {day && month && year
-              ? `${year}-${month.padStart?.(2, "0")}-${day.padStart?.(2, "0")}`
-              : "Incomplete"}
-          </p>
-        </div>
+        <p>
+          Selected date:{" "}
+          {day && month && year
+            ? `${year}-${month.padStart?.(2, "0")}-${day.padStart?.(2, "0")}`
+            : "Incomplete"}
+        </p>
+      </div>
 
-        <button type="submit">S'enregistrer</button>
-      </form>
+      <button type="submit">S'enregistrer</button>
     </div>
   );
 }
